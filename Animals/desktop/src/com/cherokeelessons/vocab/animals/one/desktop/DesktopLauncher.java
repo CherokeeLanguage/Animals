5ae46b1058084b92828893ffe0ce7bf46e8d6996
mjoyner <mjoyner@vbservices.net>
2016-04-27 23:04:26 -0400

    switch to Esperanto Animals source code to retrofit back as Cherokee Animals.

--- Animals/desktop/src/com/cherokeelessons/vocab/animals/one/desktop/DesktopLauncher.java
+++ Animals/desktop/src/com/cherokeelessons/vocab/animals/one/desktop/DesktopLauncher.java
@@ -4,18 +4,27 @@ import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.cherokeelessons.vocab.animals.one.CherokeeAnimals;
-import com.cherokeelessons.vocab.animals.one.DisplaySize;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+		float width = LwjglApplicationConfiguration.getDesktopDisplayMode().width;
+		if (width<1) {
+			width = CherokeeAnimals.size.size().width;
+		}
+		float height = LwjglApplicationConfiguration.getDesktopDisplayMode().height;
+		if (height<1) {
+			height = CherokeeAnimals.size.size().height;
+		}
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.allowSoftwareMode=true;
+		config.audioDeviceBufferSize=4*1024;
+		config.audioDeviceBufferCount=4;
 		config.audioDeviceSimultaneousSources=4;
 		config.forceExit=true;
-		config.height=DisplaySize._720p.height();
+		config.height=(int)(height*.9f);
 		config.resizable=true;
 		config.title="Cherokee Animals";
-		config.width=DisplaySize._720p.width();
+		config.width=(int)(width*.9f);
 		config.addIcon("icon/icon128.png", FileType.Internal);
 		config.addIcon("icon/icon32.png", FileType.Internal);
 		config.addIcon("icon/icon16.png", FileType.Internal);
