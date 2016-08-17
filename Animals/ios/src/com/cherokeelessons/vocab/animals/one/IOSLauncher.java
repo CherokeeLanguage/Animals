91e75ce6c83ff89b78d050d9ab7437bd9b06c5dc
mjoyner <mjoyner@vbservices.net>
2016-08-17 18:05:15 -0400

    aaa

--- Animals/ios/src/com/cherokeelessons/vocab/animals/one/IOSLauncher.java
+++ Animals/ios/src/com/cherokeelessons/vocab/animals/one/IOSLauncher.java
@@ -11,12 +11,18 @@ public class IOSLauncher extends IOSApplication.Delegate {
     @Override
     protected IOSApplication createApplication() {
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        config.allowIpod=true;
+        config.orientationLandscape=true;
+        config.orientationPortrait=false;
+        
         return new IOSApplication(new CherokeeAnimals(), config);
     }
 
     public static void main(String[] argv) {
         NSAutoreleasePool pool = new NSAutoreleasePool();
+        System.out.println("STARTUP");
         UIApplication.main(argv, null, IOSLauncher.class);
+        System.out.println("SHUTDOWN");
         pool.close();
     }
 }
