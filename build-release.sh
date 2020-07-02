1bae7535463644f030dc81c6b8d91093e51e8a4e
Michael Conrad <m.conrad.202@gmail.com>
2020-07-02 14:30:49 -0400

    Update build release script.

--- build-release.sh
+++ build-release.sh
@@ -7,25 +7,40 @@ trap 'echo ERROR' ERR
 
 cd "$(dirname "$0")"
 
+if ! git diff-index --quiet HEAD --; then
+    git status
+    echo
+    echo "PENDING CHANGES NOT COMMITTED - ABORTING [post deck rebuild]"
+    echo
+    exit -1
+fi
+
 #Ensure the project can be built before doing anything else.
 ./gradlew clean || exit 1
 ./gradlew core:build || exit 1
 ./gradlew desktop:dist || exit 1
 ./gradlew android:assembleRelease || exit 1
 
+if ! git diff-index --quiet HEAD --; then
+    git status
+    echo
+    echo "PENDING CHANGES NOT COMMITTED - ABORTING [post project test full rebuild]"
+    echo
+    exit -1
+fi
+
 #Make sure we have an up-to-date git log in the text folder as "git-changelog.txt"
 git log --simplify-merges --pretty=format:"%ad [%h]:%d %s" --abbrev-commit --date=short > android/assets/text/git-changelog.txt
 git add android/assets/text/git-changelog.txt
 git commit android/assets/text/git-changelog.txt -m "Update git changelog." || true
 
-git add .
-git commit -a -m "Autocommit for next release build." || true
-
 version=$(head -n1 version)
 version=$(($version + 1 ))
 xversion="${version:0:${#version}-2}.${version: -2}"
 
+echo "==================================="
 echo "BUILD RELEASE: $xversion ($version)"
+echo "-----------------------------------"
 
 sed -i "s/version = '.*'/version = '$xversion'/g" build.gradle
 sed -i "s/versionCode=\".*\"/versionCode=\"$version\"/g" android/AndroidManifest.xml
@@ -34,11 +49,9 @@ sed -i "s/app.version=.*$/app.version=$xversion/g" ios/robovm.properties
 
 echo "$version" > version
 
-git add .
+git add version
 git commit -a -m "Bump version for release build." || true
 git tag "${xversion}" || true
-git push --all
-git push --tags
 
 #Build the newly tagged version.
 ./gradlew clean
