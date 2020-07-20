dcbe34c21c58b30e826a2ab28978cc05d511ead0
Michael Conrad <m.conrad.202@gmail.com>
2020-07-20 08:31:51 -0400

    audio updates

--- espeak-ng/sync-mp3s.sh
+++ espeak-ng/sync-mp3s.sh
@@ -10,10 +10,10 @@ trap 'echo ERROR; read a' ERR
 cd "$(dirname "$0")"
 
 SRC="mp3/"
-DEST="../android/assets/mp3-challenges/"
+DEST="../android/assets/audio/challenges/"
 
 echo " - Syncing android assets folder with any changed mp3s."
 
-rsync -a --delete-after --human-readable --progress "$SRC" "$DEST"
+rsync -a --human-readable --progress "$SRC" "$DEST"
 
 exit 0
