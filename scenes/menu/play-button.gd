9191869e50dc23ed03a8707bd5be44ecfd84641e
Michael Conrad <m.conrad.202@gmail.com>
2021-11-14 12:15:39 -0500

    initial main menu displayed

---
+++ scenes/menu/play-button.gd
@@ -0,0 +1,21 @@
+extends "res://ui-nodes/UI_Button.gd"
+
+
+func _ready():
+	
+	# needed for gamepads to work
+	grab_focus()
+
+
+func _on_Button_pressed():
+	var params = {
+		show_progress_bar = false,
+		"a_number": 10,
+		"a_string": "Ciao mamma!",
+		"an_array": [1, 2, 3, 4],
+		"a_dict": {
+			"name": "test",
+			"val": 15
+		},
+	}
+	Game.change_scene("res://scenes/gameplay/gameplay.tscn", params)
---
+++ ui-nodes/UI_Button.gd
@@ -0,0 +1,10 @@
+extends Button
+
+
+func _ready():
+	# needed for gamepads to work
+	grab_focus()
+
+
+func _on_Button_pressed():
+	pass
